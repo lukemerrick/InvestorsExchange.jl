46bdb66b07b25ef42f2782aadd92f0e18349fa56
Luke Merrick <contact@lukemerrick.com>
2021-01-14 15:32:53 -0800

    Add usage example

--- README.md
+++ README.md
@@ -15,3 +15,14 @@ Inspired by [this Python implementation](https://github.com/vfrazao-ns1/IEXTools
 ### DEEP Feed
 * TradeReportMessage message type only
 * Untested
+
+## Usage
+The package is still in early shape, but I've included the trade download script this package is built to power to show a usage example. You can run this script like this:
+```bash
+julia --threads <number of CPU cores on your machine> trade_download_script.jl /path/to/save_dir
+```
+
+If you don't care about taking advantage of multi-threading or specifying a custom save directory (the default is `./trade_data`), you can just run `julia trade_download_script.jl`.
+
+### Download script details
+In the download script, I avoid downloading more than a handful of raw PCAP data files to disk by running downloads [asynchronously](https://docs.julialang.org/en/v1/manual/asynchronous-programming/) with downloaded filenames piped into a limited-sized [Channel](https://docs.julialang.org/en/v1/base/parallel/#Base.Channel). As downloads complete, the file paths are consumed by [multi-threaded](https://docs.julialang.org/en/v1/manual/multi-threading/) parsing code that reads the `TradeReportMessage` messages, organizes them into a Julia `DataTable`, and writes them to disk in parquet format. To take advantage of this parallelization and speed up the parsing of literally every TOPS feed message that IEX has issued since mid 2017, it is recommended you include the `--threads` flag.
---
+++ trade_download_script.jl
@@ -0,0 +1,90 @@
+"""
+Example demonstrating the usage of the IEX package.
+
+Downloads historical TOPS feeds from IEX and extracts trade events to parquet files.
+
+Change the PARQUET_DIR constant to adjust the output file.
+"""
+
+import Base: Threads
+import IEX
+import Logging
+import Parquet: write_parquet
+import Pkg
+
+# add additional non-IEX dependencies
+Pkg.add(["Logging", "Parquet"])
+
+# this is the output directory for parquet files containing the trade messages
+#  defaults to "./trade_data" if no argument passed
+const PARQUET_DIR = isempty(ARGS) ? "./trade_data" : ARGS[1]
+
+# set up console loggingogging
+logger = Logging.ConsoleLogger()
+Logging.global_logger(logger)
+
+# define methods for downloading and parsing PCAP files in an efficient, parallelized manner
+function rewrite_trades(in_filepath::String, out_filepath::String)::String
+    trade_messages = IEX.read_trade_report_messages(in_filepath; show_progress_bar=false)
+    rm(in_filepath)
+    df = IEX.assemble_trade_report_frame(trade_messages)
+    write_parquet(out_filepath, df)
+    return out_filepath
+end
+
+function async_download(download_links, out_dir, out_filepath_channel)
+    for link in download_links
+        @Logging.info "Downloading from $link"
+        filepath = IEX.download_file(link, out_dir; show_progress_bar=false)
+        @Logging.info "Completed download of $filepath"
+        push!(out_filepath_channel, filepath)
+    end
+    # signal the end
+    push!(out_filepath_channel, nothing)
+end
+
+#### actually kick off the downloading! ####
+try mkdir(PARQUET_DIR) catch end
+
+# get list of all TOPS 1.6 download links, filtering out those already downloaded
+downloaded_dates = Set(filepath[1:8] for filepath in readdir(PARQUET_DIR))
+pay = IEX.query_hist_filenames()
+download_links = String[]
+dates = sort(collect(keys(pay)); rev=true)
+for d in dates
+    if d in downloaded_dates
+        continue
+    end
+    for blob in pay[d]
+        if blob["feed"] == "TOPS" && blob["version"] == "1.6"
+            push!(download_links, blob["link"])
+        end
+    end
+end
+@Logging.info "Found $(length(download_links)) un-downloaded days to download."
+
+# Using an async process, download files up to a maximum of K waiting to be processed
+dl_filepaths = Channel(2)  # Threads.nthreads() + 2 files on disk in worst case
+pcap_dir = joinpath(tempdir(), "tmp_iex_download")
+try mkdir(pcap_dir) catch end
+@Logging.info "Downloading PCAP files to $pcap_dir"
+@async async_download(download_links, pcap_dir, dl_filepaths)
+
+# using threads, read trade data from downloaded files, save trade data, and delete files
+@Logging.info "Extracting trades to parquet files in $PARQUET_DIR"
+thread_results = []
+while true
+    in_filepath = take!(dl_filepaths)
+    if in_filepath === nothing
+        break
+    end
+    out_filename = splitext(splitext(basename(in_filepath))[1])[1] * ".parquet"
+    out_filepath = joinpath(PARQUET_DIR, out_filename)
+    @Logging.info("Spawning worker thread to rewrite $in_filepath to $out_filepath")
+    res = @Threads.spawn rewrite_trades(in_filepath, out_filepath)
+    pushfirst!(thread_results, res)
+end
+while length(thread_results) > 0
+    wait(pop!(thread_results))
+end
+rm(pcap_dir, recursive=True)
