26bbc014f319b4a707729cc520da0ddb1de1463a
Luke Merrick <contact@lukemerrick.com>
2021-06-27 09:57:56 -0700

    Rewrite PCAP file parsing as an interator

--- src/read_pcap.jl
+++ src/read_pcap.jl
@@ -55,6 +55,82 @@ struct TPHeaderVaryingHalf
     send_time::Int64
 end
 
+
+struct TradeMessageIter
+    pcap_file::AbstractString
+    show_progress_bar::Bool
+    protocol_magic_bytes::UInt16
+end
+TradeMessageIter(pcap_file::AbstractString; show_progress_bar::Bool=true, protocol_magic_bytes::UInt16=TOPS_PROTOCOL_ID_1_6) = TradeMessageIter(pcap_file, show_progress_bar, protocol_magic_bytes)
+
+struct TradeMessageIterState
+    io::Union{Nothing,IO}
+    messages_left::Int
+    n_trades::Int
+    progress_bar::Union{Nothing,ProgressMeter.ProgressUnknown}
+end
+TradeMessageIterState() = TradeMessageIterState(nothing, 0, 0, nothing)
+
+
+function Base.iterate(
+    iter::TradeMessageIter,
+    state::TradeMessageIterState=TradeMessageIterState()
+)::Union{Tuple{TradeReportMessage,TradeMessageIterState},Nothing}
+    io = state.io
+    if io === nothing
+        io = GZip.open(iter.pcap_file, "r")
+    end
+
+    n_trades = state.n_trades
+    messages_left = state.messages_left
+    progress_bar = state.progress_bar
+    if iter.show_progress_bar && progress_bar === nothing
+        progress_bar = ProgressMeter.ProgressUnknown()
+    end
+
+    while !eof(io)
+        # seek for next header while no messages left
+        while messages_left == 0
+            tp_header = seek_header(io, iter.protocol_magic_bytes)
+            if tp_header === nothing
+                break
+            end
+            messages_left = tp_header.message_count
+        end
+
+        # if seeking got to the end of the file, break out
+        if eof(io)
+            break
+        end
+
+        # read messsage
+        message_size = read(io, UInt16)
+        message_type = Char(read(io, 1)[1])
+        message = nothing
+        if message_type == 'T'  # 'T' means TradeReportMessage
+            if !(message_size - 1 == sum(sizeof(x) for x in fieldtypes(TradeReportMessage)))
+                throw("WTF!")
+            end
+            message = read_struct(io, TradeReportMessage)
+            n_trades += 1
+        else
+            _ = read(io, message_size - 1)
+        end
+        messages_left -= 1
+        if progress_bar !== nothing
+            ProgressMeter.next!(progress_bar, showvalues=[("Trades", n_trades)])
+        end
+        if message !== nothing
+            new_state = TradeMessageIterState(io, messages_left, n_trades, progress_bar)
+            return message, new_state
+        end
+    end
+end
+Base.IteratorSize(iter::TradeMessageIter) = Base.SizeUnknown()
+Base.IteratorEltype(iter::TradeMessageIter) = Base.HasEltype()
+Base.eltype(iter::TradeMessageIter) = TradeReportMessage
+
+
 """Equivalent of the `read` function but for structs of fixed-size types"""
 function read_struct(io::IO, struct_type::Type)
     field_gen = (
@@ -68,7 +144,7 @@ end
 Reads PCAP file contents until header encountered.
 Returns the varying portion of the header.
 """
-function seek_header(io, protocol_id)::Union{TPHeaderVaryingHalf, Nothing}
+function seek_header(io, protocol_id)::Union{TPHeaderVaryingHalf,Nothing}
     match_bytes = [
         reinterpret(UInt8, [IEX_VERSION]);
         reinterpret(UInt8, [IEX_RESERVED]);
@@ -103,52 +179,17 @@ function read_trade_report_messages(
     protocol_magic_bytes::UInt16=TOPS_PROTOCOL_ID_1_6
 )::Vector{TradeReportMessage}
     trade_messages = TradeReportMessage[]
-    GZip.open(gzipped_pcap_filepath, "r") do io
-        progress_bar = show_progress_bar ? ProgressMeter.ProgressUnknown() : nothing
-        messages_left = 0
-        n_trades = 0
-        while !eof(io)
-            # seek for next header while no messages left
-            while messages_left == 0
-                tp_header = seek_header(io, protocol_magic_bytes)
-                if tp_header === nothing
-                    break
-                end
-                messages_left = tp_header.message_count
-            end
-
-            # if seeking got to the end of the file, break out
-            if eof(io)
-                break
-            end
-
-            # read messsage
-            message_size = read(io, UInt16)
-            message_type = Char(read(io, 1)[1])
-            if message_type == 'T'  # TradeReportMessage
-                if !(message_size - 1 == sum(sizeof(x) for x in fieldtypes(TradeReportMessage)))
-                    throw("WTF!")
-                end
-                message = read_struct(io, TradeReportMessage)
-                push!(trade_messages, message)
-                n_trades += 1
-            else
-                _ = read(io, message_size - 1)
-            end
-            messages_left -= 1
-            if n_trades + 1 % 10_000 == 0
-                @info "Have read $n_trades from $gzipped_pcap_filepath on $(Threads.threadid())"
-            end
-            if show_progress_bar
-                ProgressMeter.next!(progress_bar, showvalues=[("Trades", n_trades)])
-            end
-        end
-        if show_progress_bar
-            ProgressMeter.finish!(progress_bar)
-        end
+    trade_message_iter = TradeMessageIter(
+        gzipped_pcap_filepath;
+        show_progress_bar=show_progress_bar,
+        protocol_magic_bytes=protocol_magic_bytes
+    )
+    for message in trade_message_iter
+        push!(trade_messages, message)
     end
     return trade_messages
 end
+
 
 """Convert a list of TradeReportMessage objects into a DataFrame."""
 function assemble_trade_report_frame(trade_report_messages::Vector{TradeReportMessage})::DataFrame
